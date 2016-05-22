9de4508b9437ba6f0cacba86bf4a37663ca318ee
intrd <x@dann.com.br>
2016-05-22 08:05:27 -0300

    added adminer

---
+++ data/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DBIntrd - Simple PHP framework for SQLite3 databases. A magic way to automatically create objects and persists data at SQLite3 tables.
+* 
+* @package intrd/sqlite-dbintrd
+* @version 2.0
+* @tags php, sqlite, framework, database
+* @link http://github.com/intrd/sqlite-dbintrd
+* @author intrd (Danilo Salles) - http://dann.com.br
+* @copyright (CC-BY-SA-4.0) 2016, intrd
+* @license Creative Commons Attribution-ShareAlike 4.0 - http://creativecommons.org/licenses/by-sa/4.0
+* Dependencies: 
+* - php >=5.3.0
+*** @docbloc 1.1 */
+
+if (1==1 //1==1 = whitelist disabled
+	or $_SERVER['REMOTE_ADDR']=="192.168.0.103"){ //whitelist
+	include_once("../vendor/vrana/adminer/adminer/index.php"); //calling adminer
+}
+?>
